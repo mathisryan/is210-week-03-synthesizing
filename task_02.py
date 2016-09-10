899bd3bd990f25c5ab6ea4716fa06b10c20b1190
Amy Fong <AmyhFong@gmail.com>
2016-09-10 20:06:22 +0000

    Changed 'Spanish' to 'Flemish'.

--- task_02.py
+++ task_02.py
@@ -4,3 +4,15 @@
 
 
 import inquisition
+
+STR = 'Spanish'
+
+STR_LEN = len(STR)
+
+START_PT = inquisition.SPANISH.index('Spanish')
+
+FIRST_PART = inquisition.SPANISH[:START_PT]
+
+SECOND_PART = inquisition.SPANISH[START_PT+STR_LEN:]
+
+FLEMISH = FIRST_PART + 'Flemish' + SECOND_PART
